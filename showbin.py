ba1123b07b2d3974859c78f7a3eb1192b58951af
Dmitry Baksheev <d.baksheev@g.nsu.ru>
2023-04-26 00:29:51 +0800

    showbin.py - revisited

--- showbin.py
+++ showbin.py
@@ -12,28 +12,38 @@ logging.basicConfig(
     datefmt='%H:%M:%S',
 )
 
+
 def showbin(bin):
     with open(bin, 'rb') as b:
-        w, = struct.unpack('f', b.read(4))
-        w = int(w)
-        logging.info(f'Width: {w}')
-        h = struct.unpack(f'{w}f', b.read(4 * w))
-        logging.info(f'X ({len(h)}: {h[0]} {h[1]} ... {h[-1]})')
+        width, = struct.unpack('f', b.read(4))
+        width = int(width)
+        logging.info(f'Width: {width}')
+        header = struct.unpack(f'{width}f', b.read(4 * width))
+        logging.info(f'X ({len(header)} pts: {header[0]} {header[1]} ... {header[-1]})')
         ys = []
         vs = []
         for i in range(10000000):
-            y = b.read(4)
-            if not y:
+            y_bytes = b.read(4)
+            if not y_bytes:
                 break
-            y, = struct.unpack('f', y)
-            v = struct.unpack(f'{w}f', b.read(4 * w))
+            y, = struct.unpack('f', y_bytes)
+            values = struct.unpack(f'{width}f', b.read(4 * width))
             ys.append(y)
-            vs.append(v)
+            vs.append(values)
             if i < 5:
-                logging.info(f'Y{i}={y}: {v[0]} {v[1]} ... {v[-1]}')
-        #logging.info(f'Got {len(ys)} y-values {ys[0]} {ys[1]} ... {ys[-1]}')
-        logging.info(f'Got {len(ys)} y-values {ys[0]:.5} {ys[1]:.5} ... {ys[-1]:5} step {(ys[-1]-ys[0])/(len(ys)-1)}')
-        print([f'{y:.4}' for y in ys])
+                logging.info(f'Y{i}={y}: {values[0]} {values[1]} ... {values[-1]}')
+        logging.info(f'...')
+        logging.info(f'Y{i-1}={y}: {values[0]} {values[1]} ... {values[-1]}')
+        vs = np.array(vs)
+        logging.info(f'Values vary from {vs.min()} to {vs.max()}')
+        mean = np.mean(vs)
+        std = np.std(vs)
+        logging.info(f'Mean {mean} std_dev {std}')
+        count_below_mean = np.sum(vs < mean)
+        count_above_mean = np.sum(mean <= vs)
+        logging.info(f'Total {count_below_mean+count_above_mean} pts'
+                     f' = {count_below_mean} pts below + {count_above_mean} pts above mean')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
